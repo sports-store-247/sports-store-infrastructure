6596bb8f8f9212677cead0a44579d163d89c320a
Sean Shmilov <sean.shmelyov@gmail.com>
2026-08-03 12:24:01 +0300

    fix: use dynamic thumbprints for github oidc

--- github-oidc.tf
+++ github-oidc.tf
@@ -6,9 +6,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
-    "ab9d0263244dd0326eb67015705a667e79cfe998"
+    data.tls_certificate.github.certificates[0].sha1_fingerprint,
+    "1b511abead59c6ce207077c0bf0e0043b1382612"
   ]
 }
 
